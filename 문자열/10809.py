14561419274bc166ab440a223f184e9200a61789
ra <ra@raui-iMac.local>
2024-06-28 18:00:25 +0900

    문자열

---
+++ 문자열/10809.py
@@ -0,0 +1,5 @@
+st = input()
+alp = list(range(97, 123))
+
+for i in alp:
+    print(st.find(chr(i)), end=' ')
---
+++ 문자열/11720.py
@@ -0,0 +1,6 @@
+count = int(input())
+num = input()
+s_num = 0
+for i in range(count):
+    s_num += int(num[i])
+print(s_num)
